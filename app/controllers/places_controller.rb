d33e199309ee3c37d61bd5a170a6c8b635ebbabf
Sos524 <sauce.thach@gmail.com>
2016-05-31 21:52:59 +0000

    Lesson 22 update

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -19,6 +19,13 @@ class PlacesController < ApplicationController
   def show
   	 @place = Place.find(params[:id])
   end
+
+
+
+  def edit
+  	  @place = Place.find(params[:id])
+  end
+
   private
 
   def place_params
