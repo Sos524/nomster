23f819b3437e1c12c65861a37d0eeed49304b552
Sos524 <sauce.thach@gmail.com>
2016-05-31 23:10:34 +0000

    Lesson 23

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,7 @@
 class PlacesController < ApplicationController
  before_action :authenticate_user!, only: [:new, :create]
 	def index
-      @places = Place.all
-
+     @places = Place.all
   end
 
 
@@ -24,6 +23,12 @@ class PlacesController < ApplicationController
 
   def edit
   	  @place = Place.find(params[:id])
+  end
+
+  def update
+   @place = Place.find(params[:id])
+   @place.update_attributes(place_params)
+   redirect_to root_path
   end
 
   private
